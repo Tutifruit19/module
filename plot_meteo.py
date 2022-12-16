ae4cae712313633454478ee6a12965a9954fa199
aurelien  henon <aurelienhenon@MacBook-Pro-de-aurelien.local>
2022-12-16 16:33:38 +0100

    Option plot

--- plot_meteo.py
+++ plot_meteo.py
@@ -12,7 +12,6 @@ from matplotlib.colors import LogNorm, BoundaryNorm
 from datetime import datetime as dt
 import matplotlib as mpl
 
-
 class quick_view:
     def __init__(self,path):
         self.path = path
@@ -22,6 +21,7 @@ class quick_view:
         self.lat = self.data['lat']
         self.lon = self.data['lon']
         self.variables =  [i for i in self.data.data_vars]
+        self.save_path = input("Entrer un path pour save les figures: ")
 
     def Resume(self):
         print("Dimension time: ")
@@ -53,34 +53,38 @@ class quick_view:
         da = da.rename({'_longitude_adjusted': lon_name})
         return da
     
-    def First_plot_contour(self,variable,tps,niveau,ax):
+    def First_plot_contour(self,variable,tps,niveau,ax,proj):
         resol = '10m'  # use data at this scale
-        cmap = mpl.cm.gist_ncar
+        cmap = mpl.cm.plasma
 
             # Add coastlines
-        ax.set_extent([self.ds["lon"][0],self.ds["lon"][-1],self.ds["lat"][0],self.ds["lat"][-1]])
+        ax.set_extent([self.ds["lon"][0],self.ds["lon"][-1],self.ds["lat"][-1],self.ds["lat"][0]])
         ax.set_title(str(variable)+" "+str(niveau)+" "+str(tps))
         cs=ax.contourf(self.ds['lon'], self.ds['lat'], self.ds,
-                    transform = ccrs.PlateCarree(), cmap=cmap)
+                    transform = proj, cmap=cmap)
 
         ax.coastlines(resolution=resol, color='black', linewidth=0.5)
         bodr = cartopy.feature.NaturalEarthFeature(category='cultural', name='admin_0_boundary_lines_land', scale=resol, facecolor='none')
         ax.add_feature(bodr, linestyle='-', alpha=1, linewidth=0.5)
-        gl=ax.gridlines(crs=ccrs.PlateCarree(), linewidth=0.5, color='black', linestyle='--', draw_labels=True, y_inline=False)
+        gl=ax.gridlines(crs=proj, linewidth=0.5, color='black', linestyle='--', draw_labels=True)
         gl.top_labels=False
         gl.right_labels=False
-        gl.xlocator= mpl.ticker.FixedLocator(np.linspace(self.ds["lon"][0],self.ds["lon"][-1],18))
-        gl.ylocator= mpl.ticker.FixedLocator(np.linspace(self.ds["lat"][0],self.ds["lat"][-1],18))
+        gl.xlocator= mpl.ticker.FixedLocator(np.arange(self.ds["lon"][0],self.ds["lon"][-1]),10)
+        gl.ylocator= mpl.ticker.FixedLocator(np.arange(self.ds["lat"][-1],self.ds["lat"][0]),10)
         return cs
 
     def first_plot(self,variable,tps,niveau):
         self.ds = self.lonflip(self.data[variable].sel(level=niveau).sel(time=tps))
+        fig = plt.figure(figsize=(11,8.5), frameon=False)
         middle_lon = self.ds["lon"][len(self.ds["lon"])//2]
         middle_lat = self.ds["lat"][len(self.ds["lat"])//2]
         print("Select your projection: 1-PlateCarree, 2-AlbersEqualArea, 3-AzimuthalEquidistant, 4-LambertConformal, 5-LambertCylindrical, 6-Mercator, 7-Miller, 8-Mollweide, 9-Orthographic, 10-Orthographic, 11-Robinson, 12-Sinusoidal, 13-Stereographic, 14-TransverseMercator, 15-UTM, 16-InterruptedGoodeHomolosine, 17-RotatedPole, 18-OSGB, 19-EuroPP, 20-Geostationary, 21-NearsidePerspective, 22-Gnomonic, 23-LambertAzimuthalEqualArea, 24-NorthPolarStereo, 25-OSNI, 26-SouthPolarStereo.")
         choice_projection = input()
         if choice_projection =="1":
-            ax = plt.axes(projection=ccrs.PlateCarree())
-        cs = self.First_plot_contour(variable,tps,niveau,ax)
-        fig = plt.figure(figsize=(11,8.5), frameon=False)
-        cbar = plt.colorbar(cs, location='right',fraction=0.046, pad=0.04)
+            proj = ccrs.PlateCarree()
+        """if choice_projection =="6":
+            proj = ccrs.Stereographic()"""
+        ax = plt.axes(projection=proj)
+        cs = self.First_plot_contour(variable,tps,niveau,ax,proj)    
+        cbar = plt.colorbar(cs, location='right',fraction=0.025, pad=0.04)
+
